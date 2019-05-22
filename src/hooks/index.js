b888c1c63531d52106c2f1b296e9c5e5b2d32868
Richard Ng <richardcrng@gmail.com>
2019-05-22 17:58:15 +0100

    export useFirebaseCurrentUser

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,9 +1,11 @@
 import useFirebaseProvider from './firebaseProvider';
 import useFirebaseContext from './firebaseContext';
 import { useFirebaseDatabaseValue } from './firebaseDatabase';
+import useFirebaseCurrentUser from './firebaseUser';
 
 export {
   useFirebaseContext,
+  useFirebaseCurrentUser,
   useFirebaseDatabaseValue,
   useFirebaseProvider
 }
