cb51374ae1b356da484969dfbb2d3e219f603214
Richard Ng <richardcrng@gmail.com>
2019-05-22 18:54:17 +0100

    rename to useFirebaseUser

--- src/hooks/firebaseUser/index.js
+++ src/hooks/firebaseUser/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import useFirebaseContext from '../firebaseContext';
 
-function useFirebaseCurrentUser() {
+function useFirebaseUser() {
   const firebase = useFirebaseContext()
   const [user, setUser] = React.useState(firebase.auth().currentUser)
 
@@ -17,4 +17,4 @@ function useFirebaseCurrentUser() {
   return user
 }
 
-export default useFirebaseCurrentUser;
+export default useFirebaseUser;
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,11 @@
 import useFirebaseProvider from './firebaseProvider';
 import useFirebaseContext from './firebaseContext';
 import { useFirebaseDatabaseValue } from './firebaseDatabase';
-import useFirebaseCurrentUser from './firebaseUser';
+import useFirebaseUser from './firebaseUser';
 
 export {
   useFirebaseContext,
-  useFirebaseCurrentUser,
+  useFirebaseUser,
   useFirebaseDatabaseValue,
   useFirebaseProvider
 }
