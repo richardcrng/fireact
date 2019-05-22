518f4efd0e20d807a927ae1b2c0871ff0259df16
Richard Ng <richardcrng@gmail.com>
2019-05-22 13:48:25 +0100

    add useFirebaseDatabaseValue hook

---
+++ src/hooks/firebaseDatabase/value/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useFirebaseContext } from "../..";
+
+function useFirebaseDatabaseValue(path) {
+  const firebase = useFirebaseContext()
+  const [value, setValue] = React.useState()
+
+  const updateValueFromSnapshotIfDifferent = React.useCallback(
+    (dataSnapshot) => {
+      const snapshotValue = dataSnapshot.value()
+      if (!_.isEqual(snapshotValue, value)) setValue(snapshotValue)
+    },
+    [value]
+  )
+
+  React.useEffect(() => {
+    const reference = firebase.database().ref(path)
+
+    reference.on('value', updateValueFromSnapshotIfDifferent)
+
+    return function cleanup() {
+      reference.off('value', updateValueFromSnapshotIfDifferent)
+    }
+  }, [updateValueFromSnapshotIfDifferent])
+}
+
+export default useFirebaseDatabaseValue;
