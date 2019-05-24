57dbe4f1c6ebe0abe2ac78b19a2b9dc265634808
Richard Ng <richardcrng@gmail.com>
2019-05-24 11:31:37 +0100

    refactor useFirebaseDatabaseValue with useStateHandlers

--- src/hooks/firebaseDatabase/value/index.js
+++ src/hooks/firebaseDatabase/value/index.js
@@ -2,29 +2,27 @@ import _ from 'lodash';
 import * as R from 'ramda'
 import React from 'react';
 import { useFirebaseContext } from "../../../hooks";
+import useStateHandlers from '../../stateHandlers';
 
 function useFirebaseDatabaseValue(path) {
   const firebase = useFirebaseContext()
 
-  const [value, setValue] = React.useState()
+  const [value, { set: setValue }] = useStateHandlers()
 
-  const updateValueFromSnapshotIfDifferent = React.useCallback(
-    (dataSnapshot) => {
-      const snapshotValue = dataSnapshot.val()
-      if (R.equal(value, snapshotValue)) setValue(snapshotValue)
-    },
-    [value]
+  const setValueFromSnapshot = React.useCallback(
+    dataSnapshot => setValue(dataSnapshot.val()),
+    [setValue]
   )
 
   React.useEffect(() => {
     const reference = firebase.database().ref(path)
 
-    reference.on('value', updateValueFromSnapshotIfDifferent)
+    reference.on('value', (setValueFromSnapshot))
 
     return function cleanup() {
-      reference.off('value', updateValueFromSnapshotIfDifferent)
+      reference.off('value', setValueFromSnapshot)
     }
-  }, [updateValueFromSnapshotIfDifferent])
+  }, [setValue])
 
   return value
 }
