8769b88a628fbd8fe703e914b0e253b775d15c5f
Richard Ng <richardcrng@gmail.com>
2019-05-24 11:32:08 +0100

    export useStateHandlers

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,10 +2,12 @@ import useFirebaseProvider from './firebaseProvider';
 import useFirebaseContext from './firebaseContext';
 import { useFirebaseDatabaseValue } from './firebaseDatabase';
 import useFirebaseUser from './firebaseUser';
+import useStateHandlers from './stateHandlers';
 
 export {
   useFirebaseContext,
   useFirebaseUser,
   useFirebaseDatabaseValue,
-  useFirebaseProvider
+  useFirebaseProvider,
+  useStateHandlers
 }
