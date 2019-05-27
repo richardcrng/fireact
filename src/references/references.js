fcad0631042c821516efceebc6df8e2a00c27c34
Richard Ng <richardcrng@gmail.com>
2019-05-27 08:15:21 +0100

    wip

--- src/references/references.js
+++ src/references/references.js
@@ -3,3 +3,7 @@ import * as R from 'ramda'
 export const getEntityTypeById = R.curry((firebase, entityType, id) => (
   firebase.database().ref(`${entityType}/${id}`)
 ))
+
+export const getChildOfRef = R.curry((reference, child) => (
+  reference.child(child)
+))
