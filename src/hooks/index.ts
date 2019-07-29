ab0994be13aea28efc89e075a8416523d3145f68
Richard Ng <richardcrng@gmail.com>
2019-07-29 15:05:04 +0100

    export hooks typescript

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,9 @@
 import useFirebase from './useFirebase';
 import useFirebaseCurrentUser from './useFirebaseCurrentUser';
 import useFirebaseDatabaseValue from './useFirebaseDatabaseValue';
-import useFirebaseProvider from './useFirebaseProvider';
 
 export {
   useFirebase,
   useFirebaseCurrentUser,
   useFirebaseDatabaseValue,
-  useFirebaseProvider
 }
