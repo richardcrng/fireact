f17c2bfb931e917ee801e8abbcdf71096bc5afcb
Richard Ng <richardcrng@gmail.com>
2019-07-29 15:02:13 +0100

    use typescript in useFirebaseDatabaseValue

--- src/hooks/useFirebaseDatabaseValue/useFirebaseDatabaseValue.ts
+++ src/hooks/useFirebaseDatabaseValue/useFirebaseDatabaseValue.ts
@@ -3,7 +3,19 @@ import React from 'react';
 import useFirebase from '../useFirebase';
 import useStateHandlers from '../useStateHandlers';
 
-function useFirebaseDatabaseValue(path, {
+interface DatabaseValueOptions {
+  orderByChild?: string
+  orderByKey?: boolean
+  orderByValue?: boolean
+  orderByPriority?: boolean
+  limitToFirst?: number
+  limitToLast?: number
+  startAt?: number | string | boolean
+  endAt?: number | string | boolean
+  equalTo?: any
+}
+
+function useFirebaseDatabaseValue<T>(path: string, {
   orderByChild,     // string
   orderByKey,       // boolean
   orderByValue,     // boolean
@@ -13,10 +25,10 @@ function useFirebaseDatabaseValue(path, {
   startAt,          // number | string | boolean
   endAt,            // number | string | boolean
   equalTo           // any
-} = {}) {
+} : DatabaseValueOptions = {}) {
   const firebase = useFirebase()
 
-  const [value, { set: setValue }] = useStateHandlers()
+  const [value, { set: setValue }] = useStateHandlers<T>()
 
   const setValueFromSnapshot = React.useCallback(
     dataSnapshot => setValue(dataSnapshot.val()),
@@ -24,37 +36,38 @@ function useFirebaseDatabaseValue(path, {
   )
 
   React.useEffect(() => {
-    let reference = firebase.database().ref(path)
+    if (firebase) {
+      let reference: firebase.database.Reference | firebase.database.Query = firebase.database().ref(path)
 
-    // order-by methods
-    if (orderByChild) {
-      reference = reference.orderByChild(orderByChild)
-    } else if (orderByKey) {
-      reference = reference.orderByKey()
-    } else if (orderByValue) {
-      reference = reference.orderByValue()
-    } else if (orderByPriority) {
-      reference = reference.orderByPriority()
-    }
+      // order-by methods
+      if (orderByChild) {
+        reference = reference.orderByChild(orderByChild)
+      } else if (orderByKey) {
+        reference = reference.orderByKey()
+      } else if (orderByValue) {
+        reference = reference.orderByValue()
+      } else if (orderByPriority) {
+        reference = reference.orderByPriority()
+      }
 
-    // query methods
-    if (limitToFirst) {
-      reference = reference.limitToFirst(limitToFirst)
-    } else if (limitToLast) {
-      reference = reference.limitToLast(limitToLast)
-    } else if (startAt) {
-      reference = reference.startAt(startAt)
-    } else if (endAt) {
-      reference = reference.endAt(endAt)
-    } else if (!_.isNil(equalTo)) { // so you can filter by falsey value
-      reference = reference.equalTo(equalTo)
-    }
+      // query methods
+      if (limitToFirst) {
+        reference = reference.limitToFirst(limitToFirst)
+      } else if (limitToLast) {
+        reference = reference.limitToLast(limitToLast)
+      } else if (startAt) {
+        reference = reference.startAt(startAt)
+      } else if (endAt) {
+        reference = reference.endAt(endAt)
+      } else if (!_.isNil(equalTo)) { // so you can filter by falsey value
+        reference = reference.equalTo(equalTo)
+      }
 
+      reference.on('value', setValueFromSnapshot)
 
-    reference.on('value', setValueFromSnapshot)
-
-    return function cleanup() {
-      reference.off('value', setValueFromSnapshot)
+      return function cleanup() {
+        reference.off('value', setValueFromSnapshot)
+      }
     }
   }, [firebase, path, setValue])
 
